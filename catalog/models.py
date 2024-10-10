f548c46e76721c5da5242c066d0e18836697a535
remote <remote>
2024-10-10 10:29:52 +0300

    Change(check admin)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    surname = models.CharField(max_length=30, unique=True) 
+
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    desc = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE,)
+    year = models.CharField(max_length=4)
